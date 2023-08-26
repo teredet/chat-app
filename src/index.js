f484f013b9b17930ef6f666584453691bfbd1a88
teredet <artem.vanzytler@gmail.com>
2023-08-26 14:11:30 +0300

    Init commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const app = express();
+app.use(express.static('public'));
+app.listen(process.env.PORT, () => console.log(`Server is up on port: ${process.env.PORT}`))
+
+
+
+
+
+
+
+
+
