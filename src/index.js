387219c7f0086c8da2758a57f5c26683829d5003
teredet <artem.vanzytler@gmail.com>
2023-08-27 10:07:17 +0300

    Setuped socket.io

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from "express";
+import http from 'http';
+import { Server } from 'socket.io';
 
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static('public'));
-app.listen(process.env.PORT, () => console.log(`Server is up on port: ${process.env.PORT}`))
 
+io.on('connection', () => console.log('New WebSocket connection'))
 
-
-
-
-
-
-
-
+server.listen(process.env.PORT, () => console.log(`Server is up on port: ${process.env.PORT}`))
