54c753769bbcc3dd2dc99a61aabfe4143e96767c
teredet <artem.vanzytler@gmail.com>
2023-08-27 14:49:06 +0300

    Added sending message

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket = io();
+
+function sendMessage(e) {
+    e.preventDefault()
+    console.log(e.target.message.value)
+    socket.emit('sendMessage', e.target.message.value)
+}
+
+
+const messageP = document.getElementById('message');
+
+socket.on('message', (message) => {
+    messageP.innerText = message;
+})
--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-io.on('connection', () => console.log('New WebSocket connection'))
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    })
+})
 
 server.listen(process.env.PORT, () => console.log(`Server is up on port: ${process.env.PORT}`))
