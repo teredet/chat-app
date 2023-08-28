e9f61a6e951888e05c142d4a2189b5d548c067be
teredet <artem.vanzytler@gmail.com>
2023-08-28 08:52:29 +0300

    Added sharing location and disconnecting messages

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,16 @@ function sendMessage(e) {
     socket.emit('sendMessage', e.target.message.value)
 }
 
+function sendLocation() {
+    if(!navigator.geolocation) return console.log("Geolocation isn't supported");
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        socket.emit('sendLocation', {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+        })
+    });
+}
 
 const messageP = document.getElementById('message');
 
--- src/index.js
+++ src/index.js
@@ -12,9 +12,18 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    })
+
+    socket.on('sendLocation', (locObj) => {
+        socket.broadcast.emit('message', `http://google.com/maps?q=${locObj.latitude},${locObj.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left.')
     })
 })
 
